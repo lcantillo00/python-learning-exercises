72836b7fbf36e4ea8bc8a88a79089e25dbbd4dd3
FVI <test@examle.com>
2017-06-19 21:52:06 -0400

    learning string, exercises

---
+++ ex14.py
@@ -0,0 +1,21 @@
+#What is the result of each of the following:
+
+#‘Python’[1]
+#“Strings are sequences of characters.”[5]
+#len(“wonderful”)
+#‘Mystery’[:4]
+#‘p’ in ‘Pineapple’
+#‘apple’ in ‘Pineapple’
+#‘pear’ not in ‘Pineapple’
+#‘apple’ > ‘pineapple’
+#‘pineapple’ < ‘Peach’
+
+‘Python’[1] = ‘y’
+‘Strings are sequences of characters.’[5] = ‘g’
+len(‘wonderful’) = 9
+‘Mystery’[:4] = ‘Myst’
+‘p’ in ‘Pineapple’ = True
+‘apple’ in ‘Pineapple’ = True
+‘pear’ not in ‘Pineapple’ = True
+‘apple’ > ‘pineapple’ = False
+‘pineapple’ < ‘Peach’ = False
---
+++ ex15.py
@@ -0,0 +1,5 @@
+#Write a function that will return the number of digits in an integer.
+def intergerNum(num):
+   nstring=str(num)
+   return len(nstring)
+print(intergerNum(34))
